994c4f6e0b8d4521c41a6582d0f05a2dbf9dd160
Faissal Elamraoui <amr.faissal@gmail.com>
2016-09-05 23:03:38 +0200

    Added directions_spec

---
+++ spec/googlemaps/services/directions_spec.rb
@@ -0,0 +1,18 @@
+require 'googlemaps/services/client'
+require 'googlemaps/services/directions'
+
+include GoogleMaps::Services
+
+describe Directions do
+  let (:directions) {
+    client = GoogleClient.new(key: "AIzadGhpcyBpcyBhIGtleQ==")
+    Directions.new(client)
+  }
+
+  describe "#query" do
+    it "returns directions between two points" do
+      allow(directions).to receive(:query).and_return("{}")
+      expect(directions.query(origin: "", destination: "")).to eq("{}")
+    end
+  end
+end
