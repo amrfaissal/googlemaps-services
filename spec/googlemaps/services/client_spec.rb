b27797cd499d838cd11dd7c7f6f3803c0dad1b39
Faissal Elamraoui <amr.faissal@gmail.com>
2016-09-05 23:02:53 +0200

    Added client_spec

---
+++ spec/googlemaps/services/client_spec.rb
@@ -0,0 +1,118 @@
+require 'googlemaps/services/client'
+require 'googlemaps/services/exceptions'
+
+include GoogleMaps::Services
+include GoogleMaps::Services::Exceptions
+
+
+describe GoogleClient do
+
+  describe "#get" do
+    context "given no API key" do
+      let (:client) { GoogleClient.new(key: nil) }
+      it "raises an error" do
+        expect {
+          client.get(url: "/path/to/service", params: {})
+        }.to raise_error(StandardError)
+      end
+    end
+
+    context "given an API key that does not start with 'AIza'" do
+      let (:client) { GoogleClient.new(key: "dGhpcyBpcyBhIGtleQ==") }
+      it "raises an error" do
+        expect {
+          client.get(url: "/path/to/service", params: {})
+        }.to raise_error(StandardError)
+      end
+    end
+
+    context "given a channel with no client ID" do
+      let (:client) { GoogleClient.new(key: "AIzadGhpcyBpcyBhIGtleQ==", channel: "chan_attr") }
+      it "raises an error" do
+        expect {
+          client.get(url: "/path/to/service", params: {})
+        }.to raise_error(StandardError)
+      end
+    end
+
+    context "given a non-alphanumeric channel string" do
+      let (:client) { GoogleClient.new(key: "AIzadGhpcyBpcyBhIGtleQ==", channel: "chan_attr") }
+      it "raises an error" do
+        expect {
+          client.get(url: "/path/to/services", params: {})
+        }.to raise_error(StandardError)
+      end
+    end
+
+    context "given a timeout with either connect_timeout or read_timeout" do
+      let (:client) { GoogleClient.new(key: "AIzadGhpcyBpcyBhIGtleQ==", timeout: 60, connect_timeout: 30) }
+      it "raises an error" do
+        expect {
+          client.get(url: "/path/to/services", params: {})
+        }.to raise_error(StandardError)
+      end
+    end
+  end
+
+  describe "#get_json_body" do
+    let (:client) { GoogleClient.new(key: "AIzadGhpcyBpcyBhIGtleQ==") }
+
+    context "given a response with status code 302" do
+      let (:resp) {
+        hash = {"location" => "https://rubygems.org", "code" => "302"}
+        hash.extend(HashDot)
+        hash
+      }
+      it "it returns a URL string" do
+        expect(client.send(:get_json_body, resp)).to eql("https://rubygems.org")
+      end
+    end
+
+    context "given a response with status code different than 200" do
+      let (:resp) {
+        hash = {"body" => "{}", "code" => "400"}
+        hash.extend(HashDot)
+        hash
+      }
+      it "raises an HTTPError" do
+        expect {
+          client.send(:get_json_body, resp)
+        }.to raise_error(HTTPError)
+      end
+    end
+
+    context "given a malformed JSON response" do
+      let (:resp) {
+        hash = {"body" => "random response", "code" => "200"}
+        hash.extend(HashDot)
+        hash
+      }
+      it "raises an APIError" do
+        expect {
+          client.send(:get_json_body, resp)
+        }.to raise_error(APIError)
+      end
+    end
+  end
+
+  describe "#generate_auth_url" do
+    context "given no API key" do
+      let (:client) { GoogleClient.new(key: nil) }
+      it "raises an error" do
+        expect {
+          client.send(:generate_auth_url, "/path/to/service", false)
+        }.to raise_error(StandardError)
+      end
+    end
+
+    context "given a path with parameters" do
+      let (:client) { GoogleClient.new(key: "AIzadGhpcyBpcyBhIGtleQ==") }
+      it "returns an auth URL with encoded key and parameters" do
+        expect(
+          client.send(:generate_auth_url, "/path/to/service", {"param1" => "value"}, false)
+        ).to eql("/path/to/service?param1=value&key=AIzadGhpcyBpcyBhIGtleQ%3D%3D")
+      end
+    end
+  end
+
+end
