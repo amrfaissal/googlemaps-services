6729a1968ec1f6f3d72a886ab0a5d101f0135518
Faissal Elamraoui <amr.faissal@gmail.com>
2016-11-01 22:56:18 +0100

    fixed a hidden bug in distancematrix service

--- lib/googlemaps/services/distancematrix.rb
+++ lib/googlemaps/services/distancematrix.rb
@@ -1,9 +1,8 @@
-require "googlemaps/services/util"
+require 'googlemaps/services/util'
 
 module GoogleMaps
   module Services
-
-    $AVOIDS = ["tolls", "highways", "ferries"]
+    AVOIDS = %w(tolls highways ferries)
 
     # Performs requests to the Google Maps Distance Matrix API.
     #
@@ -41,57 +40,53 @@ module GoogleMaps
                 units: nil, departure_time: nil, arrival_time: nil, transit_mode: nil,
                 transit_routing_preference: nil, traffic_model: nil)
         params = {
-          "origins" => Convert.piped_location(origins),
-          "destinations" => Convert.piped_location(destinations)
+            'origins' => Convert.piped_location(origins),
+            'destinations' => Convert.piped_location(destinations)
         }
 
         if mode
-          if !$TRAVEL_MODES.include? mode
-            raise StandardError, "Invalid travel mode."
-          end
-          params["mode"] = mode
+          raise StandardError, 'Invalid travel mode.' unless TRAVEL_MODES.include? mode
+          params['mode'] = mode
         end
 
         if language
-          params["language"] = language
+          params['language'] = language
         end
 
         if avoid
-          if !$AVOIDS.include? avoid
-            raise StandardError, "Invalid route restriction."
-          end
-          params["avoid"] = avoid
+          raise StandardError, 'Invalid route restriction.' unless AVOIDS.include? avoid
+          params['avoid'] = avoid
         end
 
         if units
-          params["units"] = units
+          params['units'] = units
         end
 
         if departure_time
-          params["departure_time"] = Convert.unix_time(departure_time)
+          params['departure_time'] = Convert.unix_time(departure_time)
         end
 
         if arrival_time
-          params["arrival_time"] = Convert.unix_time(arrival_time)
+          params['arrival_time'] = Convert.unix_time(arrival_time)
         end
 
         if departure_time && arrival_time
-          raise StandardError, "Should not specify both departure_time and arrival_time."
+          raise StandardError, 'Should not specify both departure_time and arrival_time.'
         end
 
         if transit_mode
-          params["transit_mode"] = Convert.join_arrayt("|", transit_mode)
+          params['transit_mode'] = Convert.join_array('|', transit_mode)
         end
 
         if transit_routing_preference
-          params["transit_routing_preference"] = transit_routing_preference
+          params['transit_routing_preference'] = transit_routing_preference
         end
 
         if traffic_model
-          params["traffic_model"] = traffic_model
+          params['traffic_model'] = traffic_model
         end
 
-        self.client.get(url: "/maps/api/distancematrix/#{self.client.response_format}", params: params).class
+        self.client.get(url: "/maps/api/distancematrix/#{self.client.response_format}", params: params)
       end
     end
   end
--- spec/googlemaps/services/distancematrix_spec.rb
+++ spec/googlemaps/services/distancematrix_spec.rb
@@ -5,14 +5,14 @@ include GoogleMaps::Services
 
 describe DistanceMatrix do
   let (:distancematrix) {
-    client = GoogleClient.new(key: "AIzadGhpcyBpcyBhIGtleQ==")
+    client = GoogleClient.new(key: 'AIzadGhpcyBpcyBhIGtleQ==')
     DistanceMatrix.new(client)
   }
 
-  describe "#query" do
-    it "returns distance matrix between origins and destinations" do
-      allow(distancematrix).to receive(:query).and_return("{}")
-      expect(distancematrix.query(origins: [], destinations: [])).to eq("{}")
+  describe '#query' do
+    it 'returns distance matrix between origins and destinations' do
+      allow(distancematrix).to receive(:query).and_return('{}')
+      expect(distancematrix.query(origins: [], destinations: [])).to eq('{}')
     end
   end
 end
