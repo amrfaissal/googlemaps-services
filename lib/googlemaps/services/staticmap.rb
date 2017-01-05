8f4fa24558d963a61c83c49359df384c3f8f0d32
Faissal Elamraoui <amr.faissal@gmail.com>
2017-01-05 16:44:10 +0100

    validate scale, format and maptype parameters in StaticMap service

--- lib/googlemaps/services/staticmap.rb
+++ lib/googlemaps/services/staticmap.rb
@@ -2,10 +2,23 @@ require 'googlemaps/services/util'
 
 module GoogleMaps
   module Services
+    ALLOWED_SCALES = [2, 4]
+    SUPPORTED_IMG_FORMATS = ["png32", "gif", "jpg", "jpg-baseline"]
+    SUPPORTED_MAP_TYPES = ["satellite", "hybrid", "terrain"]
 
     # Performs requests to the Google Static Map API.
     #
-    # TODO: Add example
+    # @example Get static map image
+    #   staticmap = GoogleMaps::Services::StaticMap.new(client)
+    #   map_img = staticmap.query(size: {:length => 640, :width => 400},
+    #                             center: "50.8449925,4.362961",
+    #                             maptype: "hybrid",
+    #                             zoom: 16)
+    #   # {
+    #   #     :url => "https://maps.googleapis.com/maps/api/staticmap?size=640x400&center=50.8449925%2C4.362961&zoom=16&maptype=hybrid",
+    #   #     :mime_type => "image/png",
+    #   #     :image_data => "iVBORw0KGgoAAAANSUhEUgAAAoAAAAGQCAMAAAAJLSEXAAADAFBMVEU..."
+    #   # }
     class StaticMap
 
       # @return [Symbol] The HTTP client.
@@ -15,10 +28,20 @@ module GoogleMaps
         self.client = client
       end
 
-      def query(size:, center: nil, zoom: nil, scale: nil, format: nil,
-                maptype: nil, language: nil, region: nil, markers: nil,
-                path: nil, visible: nil, style: nil)
-        params = { 'size' => size }
+      # Get the static map image.
+      #
+      # @param [Hash] size The rectangular dimensions of the map image.
+      # @param [String, Hash] center The address or lat/lng value of the map's center.
+      # @param [Integer] zoom The magnification level of the map.
+      # @param [Integer] scale The scale of the map. This affects the number of pixels that are returned.
+      # @param [String] format The format of the resulting image. Defaults to "png8" or "png".
+      # @param [String] maptype The type of map to construct. Defaults to "roadmap".
+      # @param [String] language The language to use for display of labels on map tiles.
+      # @param [String] region The region code specified as a two-character ccTLD ('top-level domain') value.
+      # @param [String, List] markers One or more markers to attach to the image at specified locations.
+      def query(size:, center: nil, zoom: nil, scale: 1, format: "png", maptype: "roadmap",
+                language: nil, region: nil, markers: nil,path: nil, visible: nil, style: nil)
+        params = { 'size' => Convert.rectangular_dimensions(size) }
 
         unless (center && zoom) || markers
           raise StandardError, "both center and zoom are required if markers not present"
@@ -32,16 +55,24 @@ module GoogleMaps
           params['zoom'] = zoom
         end
 
-        if scale
+        if scale != 1
+          unless ALLOWED_SCALES.include? scale
+            raise StandardError, "invalid scale value."
+          end
           params['scale'] = scale
         end
 
-        # TODO: validate the format and align with the supported formats
-        if format
+        if format != "png"
+          unless SUPPORTED_IMG_FORMATS.include? format
+            raise StandardError, "invalid image format."
+          end
           params['format'] = format
         end
 
-        if maptype
+        if maptype != "roadmap"
+          unless SUPPORTED_MAP_TYPES.include? maptype
+            raise StandardError, "invalid maptype value."
+          end
           params['maptype'] = maptype
         end
 
@@ -65,7 +96,6 @@ module GoogleMaps
           params['visible'] = visible
         end
 
-        # TODO: Create wrapper around styling the map
         if style
           params['style'] = style
         end
