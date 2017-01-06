3d220461672f3eaa69d6e350e0c4a8939ff1c36a
Faissal Elamraoui <amr.faissal@gmail.com>
2017-01-06 14:23:58 +0100

    setup code coverage

--- Gemfile
+++ Gemfile
@@ -2,4 +2,5 @@ source 'https://rubygems.org'
 
 gemspec
 
+gem 'simplecov', :require => false, :group => :test
 gem 'coveralls', require: false
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,14 @@
 require 'coveralls'
-Coveralls.wear!
+require 'simplecov'
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter
+])
+
+SimpleCov.start do
+  add_filter '/spec/'
+end
 
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 Dir['googlemaps/services/*.rb'].each { |file| require file }
